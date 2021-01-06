6a73c5b83ab7c63b509f957bc37477617fb20a89
Josanilton <josanilton.filho@gmail.com>
2021-01-06 14:19:31 -0300

    #

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Header.css';
+import logo from '../assets/imgs/calculator-logo.svg'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo"><img src={logo}/></div>
+            <h1 className="header-title">Calculator</h1>
+        </header>
+    );
+};
